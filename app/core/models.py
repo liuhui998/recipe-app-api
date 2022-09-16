8b266266d5d5da95b008884051187612d5b2d185
Peter.Liu <liuhui998@gmail.com>
2022-09-16 09:09:50 +0800

    Validate user email presence

--- app/core/models.py
+++ app/core/models.py
@@ -9,7 +9,9 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User Must have Email Address!')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
